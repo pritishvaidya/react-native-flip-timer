df633cecf1ed9b0738ce63d1bcb25a291760889e
pritishvaidya <pritishvaidya94@gmail.com>
2019-02-25 12:38:35 +0530

    Adding the flip number animations

--- src/components/flip-number.js
+++ src/components/flip-number.js
@@ -1,29 +1,80 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Animated } from 'react-native';
 import PropTypes from 'prop-types';
 
 import style from './style';
 
+import TransformUtil from '../utils';
+
 class FlipNumber extends React.Component {
-  state = {}
+  constructor(props) {
+    super(props);
+    this.rotateFront = new Animated.Value(0);
+    this.rotateBack = new Animated.Value(-180);
+
+    this.frontRef = null;
+    this.backRef = null;
+  }
+
+  componentDidMount() {
+    const { size } = this.props;
+    this.animateTick();
+    this.rotateFront.addListener(({ value }) => {
+      this.transformRef(this.frontRef, value, size * 0.3);
+    });
+
+    this.rotateBack.addListener(({ value }) => {
+      this.transformRef(this.backRef, value, -size * 0.3);
+    });
+  }
+
+  setFrontRef = (ref) => {
+    this.frontRef = ref;
+  }
+
+  setBackRef = (ref) => {
+    this.backRef = ref;
+  }
+
+  animateTick = () => {
+    Animated.parallel([
+      Animated.timing(this.rotateFront, {
+        toValue: 180,
+        duration: 1000,
+      }),
+      Animated.timing(this.rotateBack, {
+        toValue: 0,
+        duration: 1000,
+      }),
+    ]).start();
+  }
+
+  transformRef = (ref, deg, y) => {
+    const matrix = TransformUtil.createIdentityMatrix();
+    TransformUtil.translateMatrix(matrix, { x: 0, y, z: 0 });
+    TransformUtil.perspectiveMatrix(matrix, 1000);
+    TransformUtil.rotateXMatrix(matrix, deg);
+    TransformUtil.untranslateMatrix(matrix, { x: 0, y, z: 0 });
+    if (ref) {
+      ref.setNativeProps({
+        style: { transform: [{ matrix }] },
+      });
+    }
+  }
 
   render() {
     const {
       size, wrapperStyle, upperCardStyle, lowerCardStyle, numberStyle,
     } = this.props;
+
     return (
-      <View style={[style.wrapper, {
-        width: size,
-        height: size * 1.2,
-      }, wrapperStyle]}
+      <View style={[style.wrapper,
+        { width: size, height: size * 1.2, borderRadius: size / 10 },
+        wrapperStyle]}
       >
         <View style={[style.upperCard, upperCardStyle]}>
           <Text style={[style.number, {
-            transform: [{
-              translateY: size / 4.8,
-            }],
-            fontSize: size / 1.5,
-            minHeight: size / 1.5,
+            transform: [{ translateY: size / 4.8 }], fontSize: size / 1.5, minHeight: size / 1.5,
           }, numberStyle]}
           >
             {'10'}
@@ -31,23 +82,43 @@ class FlipNumber extends React.Component {
         </View>
         <View style={[style.lowerCard, lowerCardStyle]}>
           <Text style={[style.number, {
-            transform: [{
-              translateY: -(size / 3),
-            }],
-            fontSize: size / 1.5,
-            minHeight: size / 1.5,
+            transform: [{ translateY: -(size / 3) }], fontSize: size / 1.5, minHeight: size / 1.5,
           }, numberStyle]}
           >
             {'10'}
           </Text>
         </View>
+        <Animated.View
+          ref={this.setFrontRef}
+          style={[style.flipCard,
+            { top: 0, borderTopLeftRadius: size / 10, borderTopRightRadius: size / 10 }]}
+        >
+          <Text style={[style.number, {
+            transform: [{ translateY: size / 4.8 }], fontSize: size / 1.5, minHeight: size / 1.5,
+          }, numberStyle]}
+          >
+            {'10'}
+          </Text>
+        </Animated.View>
+        <Animated.View
+          ref={this.setBackRef}
+          style={[style.flipCard,
+            { top: '50%', borderBottomLeftRadius: size / 10, borderBottomRightRadius: size / 10 }]}
+        >
+          <Text style={[style.number, {
+            transform: [{ translateY: -(size / 3) }], fontSize: size / 1.5, minHeight: size / 1.5,
+          }, numberStyle]}
+          >
+            {'10'}
+          </Text>
+        </Animated.View>
       </View>
     );
   }
 }
 
 FlipNumber.defaultProps = {
-  size: 40,
+  size: 200,
   wrapperStyle: {},
   upperCardStyle: {},
   lowerCardStyle: {},
--- src/components/style.js
+++ src/components/style.js
@@ -3,7 +3,6 @@ import { StyleSheet } from 'react-native';
 export default StyleSheet.create({
   wrapper: {
     backgroundColor: '#333333',
-    overflow: 'hidden',
   },
   upperCard: {
     flex: 0.5,
@@ -25,5 +24,16 @@ export default StyleSheet.create({
     flex: 1,
     fontWeight: '700',
     color: '#cccccc',
+  },
+  flipCard: {
+    position: 'absolute',
+    left: 0,
+    height: '50%',
+    width: '100%',
+    overflow: 'hidden',
+    backgroundColor: '#333333',
+    backfaceVisibility: 'hidden',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
