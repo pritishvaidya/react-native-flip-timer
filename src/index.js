37e94a29e55f28d5b4dcdd545a374089d2b1e889
Pritish <pritishvaidya94@gmail.com>
2020-09-04 18:57:59 +0530

    Update example

--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
 export { default as Timer } from './components/timer';
 export { default as FlipNumber } from './components/flip-number';
+export { default as FlipCard } from './components/flip-number/number-card';
